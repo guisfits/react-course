4b3bdcb111b3bf19bb51f0cc8bf9f6057a5c7329
Gui Camargo <guilherme.de-camargo-silva@podium.com>
2022-11-04 10:41:01 -0300

    adding more components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Backdrop from "./components/backdrop";
+import Todo from "./components/todo"
+import Modal from "./components/modal";
+import MyBackdrop from "./components/backdrop";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React'/>
+      <Todo text='Master React'/>
+      <Todo text='Explore the full React course'/>
+      <Modal/>
+      <MyBackdrop/>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/backdrop.js
@@ -0,0 +1,3 @@
+export default function MyBackdrop() {
+  return <div className="backdrop" />;
+}
