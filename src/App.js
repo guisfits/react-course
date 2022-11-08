3981333e29bc5c586fd4f541dd14f46c4f0d27f2
Gui Camargo <guilherme.de-camargo-silva@podium.com>
2022-11-08 11:45:04 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/all_meetups";
+import NewMeetupsPage from "./pages/new_meetup";
+import FavoritesPage from "./pages/favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route path="/">
+        <AllMeetupsPage />
+      </Route>
+
+      <Route path="/new-meetup">
+        <NewMeetupsPage />
+      </Route>
+
+      <Route path="/favorites">
+        <FavoritesPage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
